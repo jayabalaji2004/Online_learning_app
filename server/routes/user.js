8384ac03416da76b3b9f11dfaaf0375a0c60de77
jayabalaji2004 <ejb16032004@gmail.com>
2024-11-18 09:58:36 +0530

    Initial Commit

---
+++ server/routes/user.js
@@ -0,0 +1,20 @@
+import express from "express";
+import {verifyUser,
+  register,loginUser,myProfile,
+  forgotPassword,resetPassword,
+} from "../controllers/user.js";
+import { isAuth } from "../middlewares/isAuth.js";
+import { addProgress, getYourProgress } from "../controllers/course.js";
+
+const router = express.Router();
+
+router.post("/user/register", register);
+router.post("/user/verify", verifyUser);
+router.post("/user/login", loginUser);
+router.get("/user/me", isAuth, myProfile);
+router.post("/user/forgot", forgotPassword);
+router.post("/user/reset", resetPassword);
+router.post("/user/progress", isAuth, addProgress);
+router.get("/user/progress", isAuth, getYourProgress);
+
+export default router;
